ae34f2f828c3b8143cca48ec599df8e4bf68e04d
philipisidro <philipisidro@gmail.com>
2022-04-08 22:00:32 -0400

    push

--- response.php
+++ response.php
@@ -1,68 +1,32 @@
 <?php
-$tryAgain = "<a href=\"http://isidrop.dev.fast.sheridanc.on.ca/a5/\"> \" Try Again\"</a>";
 
-echo $_POST;
+$email = $_POST["userEmail"];
+$phone = $_POST["userPhone"];
+$age = $_POST["userAge"];
+$password = $_POST["userPassword"];
 
-?>
 
-// checkName();
-// checkEmail();
-// checkPhone();
-// checkAge();
-// checkPassword();
 
-// function checkName(){
-//     if(empty($_POST["name"])){
-//         $errMSG = "Enter Your Name";
-//         echo $errMSG;
-//     }else if ((!preg_match("/^([a-zA-Z' ]+)$/")), (($_POST["name"])){
-//         $errMSG = "Your username must have letters only!";
-//         echo $errMSG; 
-//     }else {
-//         echo "hello " + $_POST["name"]; 
-//     }
-// }
+function validateName(){
+    $name = $_POST["userName"];
+    if (isset($name) && !empty($name)){
+        if (preg_match('[a-zA-Z]', $name)){
+            clean($name);
+            return $name
+        }else {
+            echo "<h1>Error: Username can only contain letters</h1>";
+        }
+    } else {
+        echo "<h1>Error: Input Your Name</h1>";
+    };
+}
 
-// function checkEmail(){
-//     if(empty($_POST["email"])){
-//         $errMSG = "Enter your email";
-//         echo $errMSG + $tryAgain;
-//     }else if ($_POST["email"]){
-//         $errMSG = "Not a valid email";
-//         echo $errMSG + $tryAgain;
-//     }else  {
-//         echo "Your email is" + $_POST["email"];
-//     }
-// }
 
-// function checkPhone(){
-//     if(empty($_POST["phone"])){
-//         $errMSG = "Enter your phone number";
-//         echo $errMSG + $tryAgain;
-//     }else if (!preg_match("/^[0-9]{10}+S/")($_POST["phone"])) {
-//         $errMSG  = "Not a valid phone number";
-//         echo $errMSG; 
-//     }else {
-//         echo "Your phone is" + $_POST["phone"];
-//     }
-// }
-// function checkAge(){
-//     if(empty($_POST["age"])){
-//         $errMSG = "Enter your age";
-//         echo $errMSG + $tryAgain;
-//     } else if ($_POST["age"]){
-//         $errMSG = "Age must be between 16 and 99";
-//     }else {
-//         echo "Your Age is" + $_POST["email"];
-//     }
 
-// }
 
-// function checkPassword(){
-//     if(empty($_POST["password"])){
-//         $errMSG = "Enter your password";
-//         echo $errMSG + $tryAgain;
-//     } else if ($_POST){
-//         $errMSG = "Password must be minimum 8 characters. " + $tryAgain;
-//     }
-// }
+function clean($d){
+    $data = trim($d);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+};
