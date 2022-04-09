ba7eabb6db82048f470faa62d0ebe61ca1b0769b
philipisidro <philipisidro@gmail.com>
2022-04-09 02:23:46 -0400

    push

--- response.php
+++ response.php
@@ -8,36 +8,100 @@
 </head>
 <body>
     
-    
-    
     <?php
-    $email = $_POST["userEmail"];
     $phone = $_POST["userPhone"];
     $age = $_POST["userAge"];
     $password = $_POST["userPassword"];
-    
+
     validateName();
-    
+    validateEmail();
+    validatePhone();
+    validateAge();
+    validatePassword();
+
     function validateName(){
         $name = $_POST["userName"];
+        $pattern = '/^[-a-z\d]/' ;
+        clean($name);
+        echo $name;
         if (isset($name) && !empty($name)){
-            if (preg_match('[a-zA-Z]', $name)){
-                clean($name);
+            if (preg_match($pattern, $name)){
+                echo " it worked";
                 return $name;
             }else {
                 echo "<h1>Error: Username can only contain letters</h1>";
             }
-        } else {
+        }else {
             echo "<h1>Error: Input Your Name</h1>";
         };
     }
-    
+    function validateEmail(){
+        $email = $_POST["userEmail"];
+        $pattern = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
+        clean($email);
+        echo $email;
+        if (isset($email) && !empty($email)){
+            if(preg_match($pattern, $email)){
+                echo "email worked";
+            }else {
+                echo "<h1>Error: Email is invalid</h1>";
+            }
+        }else {
+            echo "<h1>Error: Input Your Email</h1>";
+        }
+    }
+    function validatePhone(){
+        $phone = $_POST["userPhone"];
+        $pattern = '/^[0-9]{10}$/';
+        clean($phone);
+        echo $phone;
+        if (isset($phone) && !empty($phone)){
+            if(preg_match($pattern, $phone)){
+                echo "phone worked";
+            }else {
+                echo "<h1>Error: Phone is invalid</h1>";
+            }
+        }else {
+            echo "<h1>Error: Input Your Phone</h1>";
+        }
+    }
+    function validateAge(){
+        $age = $_POST["userAge"];
+        echo gettype($age);
+        clean($age);
+        if (isset($age) && !empty($age)){
+            if(($age >= 16) && ($age <= 99)){
+                echo "age worked";
+            }else {
+                echo "<h1>Error: Age is invalid </h1>";
+            }
+        }else {
+            echo "<h1>Error: Input Your Age</h1>";
+        }
+    }
+    function validatePassword(){
+        $password = $_POST["userPassword"];
+        $pattern = '/^.{8,}$/';
+        clean($password);
+        echo $password;
+        if (isset($password) && !empty($password)){
+            if(preg_match($pattern, $password)){
+                echo "password worked";
+            }else {
+                echo "<h1>Error: Password is invalid</h1>";
+            }
+        }else {
+            echo "<h1>Error: Input Your Password</h1>";
+        }
+    }
+
     function clean($d){
         $data = trim($d);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
     };
+    
     ?>
 </body>
 </html>
